64ed39819acb1a9d3d31566764cfcc68cddd9dc5
alongken2005 <360586201@qq.com>
2013-03-13 20:04:02 +0800

    更新

--- application/controllers/pay.php
+++ application/controllers/pay.php
@@ -26,11 +26,31 @@ class Pay extends CI_Controller {
 		$amount = 0;
 		write_log($pids, 'pay', 'pay');
 
-		$lists = $this->db->query('SELECT price_base FROM ab_product WHERE id IN('.$pids.')')->result_array();
+		$user = $this->db->get_data('account', array('id'=>$this->uid), 'parent_id, id')->row_array();
+
+		$lists = $this->db->query('SELECT price_base,id,sku,price_type FROM ab_product WHERE id IN('.$pids.')')->result_array();
+		$invoice_id = getId('invoice_id');
+
 		foreach($lists as $v) {
+			$invoice_item[] = array(
+				'id'				=> $item_id = getId('invoice_item'),
+				'site_id'			=> 1,
+				'date_orig'			=> $this->timestamp,
+				'parent_id'			=> $user['parent_id'],
+				'account_id'		=> $this->uid,
+				'invoice_id'		=> $invoice_id,
+				'product_id'		=> $v['id'],
+				'sku'				=> $v['sku'],
+				'quantity'			=> 1,
+				'total_amt'			=> $v['price_base'],
+				'item_type'			=> 0,
+				'price_type'		=> $v['price_type'],
+				'price_base'		=> $v['price_base'],
+				'recurr_schedule'	=> 1,
+			);
 			$amount += $v['price_base'];
 		}
-		$invoice_id = getId('invoice_id');
+
 		$invoice_data = array(
 			'id'				=> $invoice_id,
 			'site_id'			=> 1,
@@ -46,7 +66,13 @@ class Pay extends CI_Controller {
 			'total_amt'			=> $amount,
 			'ip'				=> $this->input->ip_address(),
 		);
+		write_log(debug($invoice_data, 0, 1), 'pay', 'pay');
 		$this->base->insert_data('invoice', $invoice_data);
+
+		if($invoice_item) {
+			$this->db->insert_batch('ab_invoice_item', $invoice_item);
+			write_log(debug($invoice_item, 0, 1), 'pay', 'pay');
+		}
 		output(0, array('invoid_id'=>$invoice_id, 'amount'=>$amount));
 	}
 
@@ -106,7 +132,57 @@ class Pay extends CI_Controller {
 					'checkout_plugin_id'=> $option,
 				);
 				$this->base->update_data('invoice', $updata_array, array('id'=>$invoid_id));
+				
+				$items = $this->base->get_data('invoice_item', array('id'=>$invoid_id))->result_array();
+				foreach($items as $v) {
 
+				}
+
+				$this->id = sqlGenID($db,"service");
+				$fields = Array('date_orig' 				=> time(),
+				'date_orig'					=> time(),
+				'parent_id' 				=> $this->parent_id,
+				'invoice_id'				=> $item->fields['invoice_id'],
+				'invoice_item_id' 			=> $invoice_item_id,
+				'account_id'				=> $invoice->fields['account_id'],
+				'account_billing_id'		=> $invoice->fields['account_billing_id'],
+				'product_id'				=> $item->fields['product_id'],
+				'sku' 						=> $item->fields['sku'],
+				'active'					=> $this->active,
+				'bind' 						=> $this->bind,
+				'type'						=> $this->type,
+				'queue' 					=> $this->queue,
+				'price'						=> $this->price,
+				'price_type' 				=> $item->fields['price_type'],
+				'taxable'					=> $prod->fields['taxable'],
+				'date_last_invoice' 		=> $invoice->fields['date_orig'],
+				'date_next_invoice'			=> $this->next_invoice,
+				'recur_schedule' 			=> $this->recurring_schedule,
+				'recur_type'				=> $prod->fields['price_recurr_type'],
+				'recur_weekday' 			=> $prod->fields['price_recurr_weekday'],
+				'recur_week'				=> $prod->fields['price_recurr_week'],
+				'recur_schedule_change' 	=> $prod->fields['price_recurr_schedule'],
+				'recur_cancel'				=> $prod->fields['price_recurr_cancel'],
+				'recur_modify' 				=> $prod->fields['price_recurr_modify'],
+				'group_grant'				=> $prod->fields['assoc_grant_group'],
+				'group_type' 				=> $prod->fields['assoc_grant_group_type'],
+				'group_days'				=> $prod->fields['assoc_grant_group_days'],
+				'host_server_id' 			=> $prod->fields['host_server_id'],
+				'host_provision_plugin_data'=> $prod->fields['host_provision_plugin_data'],
+				'host_ip' 					=> $this->host_ip,
+				'host_username'				=> $this->host_username,
+				'host_password' 			=> $this->host_password,
+				'domain_name'				=> $item->fields['domain_name'],
+				'domain_tld' 				=> $item->fields['domain_tld'],
+				'domain_term'				=> $item->fields['domain_term'],
+				'domain_type' 				=> $item->fields['domain_type'],
+				'domain_date_expire'		=> $this->domain_date_expire,
+				'domain_host_tld_id'		=> $this->domain_host_tld_id,
+				'domain_host_registrar_id'	=> $this->domain_host_registrar_id,
+				'prod_attr'					=> $item->fields['product_attr'],
+				'prod_attr_cart'			=> $item->fields['product_attr_cart'],
+				'prod_plugin_name' 			=> @$prod->fields["prod_plugin_file"],
+				'prod_plugin_data'			=> @$prod->fields["prod_plugin_data"]);
 				output(0, $receiptResponse);
 			}
 		}
