d3a1a84cd03a98676fd8706a832b24d8ab2c7e03
alongken2005 <360586201@qq.com>
2013-08-21 18:03:19 +0800

    更新

---
+++ application/views/default/shareapi.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>分享</title>
+	<link rel="stylesheet" type="text/css" href="<?=THEME_VIEW?>css/style.css"/>
+</head>
+<body>
+<!-- JiaThis Button BEGIN -->
+<div class="jiathis_style">
+<a class="jiathis_button_qzone"></a>
+<a class="jiathis_button_tsina"></a>
+<a class="jiathis_button_tqq"></a>
+<a class="jiathis_button_weixin"></a>
+<a class="jiathis_button_renren"></a>
+<a href="http://www.jiathis.com/share" class="jiathis jiathis_txt jiathis_separator jtico jtico_jiathis" target="_blank"></a>
+<a class="jiathis_counter_style"></a>
+</div>
+<script type="text/javascript" >
+var jiathis_config={
+	pic:"http://c.hiphotos.baidu.com/album/w%3D2048/sign=2936f51c9345d688a302b5a490fa7c1e/a50f4bfbfbedab64e98471eff636afc379311e2e.jpg",
+}
+</script>
+<script type="text/javascript" src="http://v3.jiathis.com/code/jia.js" charset="utf-8"></script>
+<!-- JiaThis Button END -->
+
+</body>
+</html>
