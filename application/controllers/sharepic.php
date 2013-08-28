a57059d5c0177713c403b4f82e6fa8f0808f8af3
alongken2005 <360586201@qq.com>
2013-08-28 21:13:26 +0800

    更新

--- application/controllers/sharepic.php
+++ application/controllers/sharepic.php
@@ -23,9 +23,10 @@ class Sharepic extends CI_Controller {
 
 	public function main() {
 		$data = file_get_contents("php://input");
-		$picName = "data/".uniqid().".jpg";
+		$picName = "data/uploads/share/".uniqid().".jpg";
 		file_put_contents($picName, $data);
-		$this->_data = array('picName'=> base_url($picName), 'content'=> rawurldecode($_GET['content']), 'title'=>rawurldecode($_GET['title']), 'url'=>rawurldecode($_GET['url']));
+		//$this->_data = array('picName'=> base_url($picName), 'content'=> rawurldecode($_GET['content']), 'title'=>rawurldecode($_GET['title']), 'url'=>rawurldecode($_GET['url']));
+		$this->_data = array('picName'=> base_url("data/uploads/share/2cd0dd49de0a648a7aa2327a6cbdcef4.jpg"), 'content'=> '今日福克斯新闻主播克莱顿莫里斯的微博透露，苹果iPhone5S的A7芯片处理速度将比当前iphone 5的A6芯片快很多，比当前一代在运算频率上快近三分之一，达到31%。于此同时来自9to5Mac的报告指出，苹果的A7处理"器采"用的是64位构架', 'title'=>"用的是64位构架", 'url'=>"http://baidu.com");
 
 		$this->load->view(THEME.'/sharepic', $this->_data);
 	}
--- application/views/default/sharepic.php
+++ application/views/default/sharepic.php
@@ -3,26 +3,29 @@
 <head>
 	<meta charset="utf-8">
 	<title>分享</title>
-	<link rel="stylesheet" type="text/css" href="<?=THEME_VIEW?>css/style.css"/>
 </head>
-<body>
-	<div>标题：<?php echo $title?></div>
-	<div>备注：<?php echo $content?></div>
-	<div>url：<?php echo $url?></div>
-	<div>图片:<img src="<?php echo $picName?>"/></div>
-<!-- JiaThis Button BEGIN -->
-<div class="jiathis_style">
-<a class="jiathis_button_qzone"></a>
-<a class="jiathis_button_tsina"></a>
-<a class="jiathis_button_tqq"></a>
-<a class="jiathis_button_weixin"></a>
-</div>
+<body style="background: #081F48 url(<?=THEME_VIEW?>/images/lake_bg.png) 0 -50px repeat-x;">
+	<div style="border: 1px solid #999;border-radius: 5px;box-shadow: 0 0 5px rgba(198,198,198,1);width: 560px; margin: 0 auto;padding: 20px;margin-top:60px; background:#FFF;">
+		<div style="overflow:hidden;zoom:1">
+			<div class="jiathis_style">
+			<b style="float:left">点击图标分享：</b>
+			<a class="jiathis_button_qzone"></a>
+			<a class="jiathis_button_tsina"></a>
+			<a class="jiathis_button_tqq"></a>
+			<a class="jiathis_button_weixin"></a>
+			</div>		
+		</div>		
+		<div style="width:540px;font-size:14px;margin-top: 10px;border: 1px solid #ccc; padding: 10px;"><?php echo $title."  ".$content?></div>
+		<div><img width="100" style="border: 2px solid #ffb941;margin-top: 10px;" src="<?php echo $picName?>"/></div>
+	</div>
+	
+
 <script type="text/javascript" >
 var jiathis_config={
-	//pic:"<?php echo $picName?>",
-	pic:"http://www.sinaimg.cn/blog/qingzujian/00820tiantangaiqing.jpg",
-	title:"<?php echo $title?>",
-	summary:"<?php echo $content?>",
+	pic:"<?php echo $picName?>",
+	//pic:"http://www.sinaimg.cn/blog/qingzujian/00820tiantangaiqing.jpg",
+	title:"<?php echo mysql_escape_string($title)?>",
+	summary:"<?php echo mysql_escape_string($content)?>",
 	url:"<?php echo $url?>",
 	appkey:{
         "tsina":"1246290740",
@@ -31,7 +34,5 @@ var jiathis_config={
 }
 </script>
 <script type="text/javascript" src="http://v3.jiathis.com/code/jia.js" charset="utf-8"></script>
-<!-- JiaThis Button END -->
-
 </body>
 </html>
